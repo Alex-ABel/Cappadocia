00652875c3ba191b3275948c3a1020660e05d210
Alex-ABel <aimner@mail.ru>
2021-10-21 15:59:23 +0300

    Coding day two is complete

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('.fairy-tail__slider, .our-trip__slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prew"><img src="img/arrow-prew.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/arrow-next.svg" alt=""></button>',
+    autoplay: true
+  });
+});
